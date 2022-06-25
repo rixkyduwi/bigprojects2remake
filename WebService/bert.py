557c72f88bdb48fa5f3f423509fcd7eb83d25bc2
Rizky Dwi Saputra <55983435+rixkyduwi@users.noreply.github.com>
2022-06-25 13:47:38 +0700

    add history for each user
    we hope this can solve the difference between UI and Web Service
    
    Co-Authored-By: FikriSyaefudin <72011837+FikriSyaefudin@users.noreply.github.com>

--- WebService/bert.py
+++ WebService/bert.py
@@ -27,14 +27,11 @@ def bert_prediction(user,context,question):
   offsetMapping = encodedData.pop("offset_mapping")[0]
   encodedData.to(device)
   model.to(device)
-
   model.eval() 
   with torch.no_grad(): # IMPORTANT! Do not computing gradient!
     outputs = model(encodedData["input_ids"], attention_mask=encodedData["attention_mask"]) # Feed forward. Without calculating loss.
-
   startLogits = outputs.start_logits[0].detach().cpu().numpy() # Getting logits, moving to CPU.
   endLogits = outputs.end_logits[0].detach().cpu().numpy() # Getting logits, moving to CPU.
-
   start_indexes = np.argsort(startLogits).tolist()
   end_indexes = np.argsort(endLogits).tolist()
   candidates = []
@@ -46,11 +43,9 @@ def bert_prediction(user,context,question):
         or offsetMapping[start_index] is None
         or offsetMapping[end_index] is None
       ):
-        continue
-      
+        continue 
       if end_index < start_index or end_index - start_index + 1 > 25:
         continue
-
       if start_index <= end_index:
         start_char = offsetMapping[start_index][0]
         end_char = offsetMapping[end_index][1]
@@ -58,13 +53,11 @@ def bert_prediction(user,context,question):
           "score": startLogits[start_index] + endLogits[end_index],
           "text": context[start_char: end_char]
         })
-
   candidates = sorted(candidates, key=lambda x: x["score"], reverse=True)[:3]
   respon_model = []
   for i, candidate in enumerate(candidates):
       dictlogs = {}
-      i=i+1
-      rank = str(i)#convert number rank to string
+      rank = str(i+1)#convert number rank to string
       score = str(candidate['score'])#convert float32 to string
       dictlogs.update({"rank": rank,"jawaban": candidate['text'], "score":score,"waktu_proses":str(time.time() - start_time)})
       respon_model.append(dictlogs)
